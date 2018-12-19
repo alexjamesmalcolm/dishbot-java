aa9f7f7430c52b61ac1bb571850d502e1854f288
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-12-18 23:09:54 -0500

    Trying to print a list or whatever

--- src/main/java/com/alexjamesmalcolm/dishbot/DishbotController.java
+++ src/main/java/com/alexjamesmalcolm/dishbot/DishbotController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
@@ -14,6 +15,8 @@ public class DishbotController {
     public void receiveMessage(HttpServletRequest request) throws IOException {
         System.out.println("Message Received");
         request.getReader().lines().forEach(System.out::println);
+        List<String> list = request.getReader().lines().collect(Collectors.toList());
+        System.out.println(list);
         String message = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
         System.out.println(message);
     }
