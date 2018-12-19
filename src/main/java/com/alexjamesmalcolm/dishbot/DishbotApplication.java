42ed01ea1552c06ed9e28e254feaa58015e9a921
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-12-18 21:21:43 -0500

    Initial commit

---
+++ src/main/java/com/alexjamesmalcolm/dishbot/DishbotApplication.java
@@ -0,0 +1,14 @@
+package com.alexjamesmalcolm.dishbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DishbotApplication.class, args);
+	}
+
+}
+
