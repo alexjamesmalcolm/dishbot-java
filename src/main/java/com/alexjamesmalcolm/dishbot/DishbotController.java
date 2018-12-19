2e5160ef0f9ea64b4a583b673289e6736681b3e4
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-12-18 22:02:57 -0500

    Printing the message received by controller

--- src/main/java/com/alexjamesmalcolm/dishbot/DishbotController.java
+++ src/main/java/com/alexjamesmalcolm/dishbot/DishbotController.java
@@ -1,9 +1,15 @@
 package com.alexjamesmalcolm.dishbot;
 
-import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.http.HttpRequest;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DishbotController {
 
+    @RequestMapping("/receive-message")
+    public void receiveMessage(HttpRequest request) {
+        System.out.println("Message Received");
+        System.out.println(request.toString());
+    }
 }
