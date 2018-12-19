07599691ce7ec35df9c65ce3e852169465a5892e
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-12-18 22:30:12 -0500

    https://codyburleson.com/getting-parts-of-a-url-from-the-httpservletrequest-object/

--- src/main/java/com/alexjamesmalcolm/dishbot/DishbotController.java
+++ src/main/java/com/alexjamesmalcolm/dishbot/DishbotController.java
@@ -1,6 +1,5 @@
 package com.alexjamesmalcolm.dishbot;
 
-import com.google.gson.Gson;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +11,25 @@ public class DishbotController {
     @RequestMapping("/receive-message")
     public void receiveMessage(HttpServletRequest request) {
         System.out.println("Message Received");
-        Gson gson = new Gson();
-        String json = gson.toJson(request);
-        System.out.println(json);
+        StringBuilder sb = new StringBuilder();
+        sb.append(" [");
+        sb.append("\n\t ContextPath: ").append(request.getContextPath());
+        sb.append("\n\t LocalAddr: ").append(request.getLocalAddr());
+        sb.append("\n\t LocalName: ").append(request.getLocalName());
+        sb.append("\n\t LocalPort: ").append(request.getLocalPort());
+        sb.append("\n\t PathInfo: ").append(request.getPathInfo());
+        sb.append("\n\t PathTranslated: ").append(request.getPathTranslated());
+        sb.append("\n\t Protocol: ").append(request.getProtocol());
+        sb.append("\n\t RemoteAddr: ").append(request.getRemoteAddr());
+        sb.append("\n\t RemoteHost: ").append(request.getRemoteHost());
+        sb.append("\n\t RemotePort: ").append(request.getRemotePort());
+        sb.append("\n\t RequestURI: ").append(request.getRequestURI());
+        sb.append("\n\t RequestURL: ").append(request.getRequestURL());
+        sb.append("\n\t Scheme: " ).append(request.getScheme());
+        sb.append("\n\t ServerName: ").append(request.getServerName());
+        sb.append("\n\t ServerPort: ").append(request.getServerPort());
+        sb.append("\n\t ServletPath: ").append(request.getServletPath());
+        sb.append("\n]");
+        System.out.println(sb);
     }
 }
