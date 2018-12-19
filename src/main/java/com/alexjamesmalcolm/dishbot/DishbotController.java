3450c38230c121f67375c2596c9c5553cbb6361c
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-12-18 22:00:05 -0500

    Created DishbotController

---
+++ src/main/java/com/alexjamesmalcolm/dishbot/DishbotController.java
@@ -0,0 +1,9 @@
+package com.alexjamesmalcolm.dishbot;
+
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DishbotController {
+
+}
