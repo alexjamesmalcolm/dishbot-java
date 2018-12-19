bfd6aeba3afa76bb9afdc799e5402fd7e13efd0e
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-12-18 23:23:18 -0500

    With the ends cut off

--- src/main/java/com/alexjamesmalcolm/dishbot/DishbotController.java
+++ src/main/java/com/alexjamesmalcolm/dishbot/DishbotController.java
@@ -1,10 +1,12 @@
 package com.alexjamesmalcolm.dishbot;
 
+import com.oracle.javafx.jmx.json.JSONException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,13 +16,9 @@ public class DishbotController {
     @RequestMapping("/receive-message")
     public void receiveMessage(HttpServletRequest request) throws IOException {
         System.out.println("Message Received");
-        request.getReader().lines().forEach(line -> {
-            System.out.println("This is a seperator");
-            System.out.println(line);
-        });
-//        List<String> list = request.getReader().lines().collect(Collectors.toList());
-//        System.out.println(list);
-//        String message = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
-//        System.out.println(message);
+        String message = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        System.out.println(message);
+        String withTheEndsCutOff = message.substring(1, message.length() - 1);
+        System.out.println(withTheEndsCutOff);
     }
 }
