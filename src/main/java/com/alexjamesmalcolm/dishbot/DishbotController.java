701efb00ef620383f9e4417d8a73880316739382
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-12-18 22:07:31 -0500

    Fixed request class

--- src/main/java/com/alexjamesmalcolm/dishbot/DishbotController.java
+++ src/main/java/com/alexjamesmalcolm/dishbot/DishbotController.java
@@ -1,14 +1,15 @@
 package com.alexjamesmalcolm.dishbot;
 
-import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class DishbotController {
 
     @RequestMapping("/receive-message")
-    public void receiveMessage(HttpRequest request) {
+    public void receiveMessage(HttpServletRequest request) {
         System.out.println("Message Received");
         System.out.println(request.toString());
     }
