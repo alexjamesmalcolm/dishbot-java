6539f080a4f317ebfeb3453403f3d4a4a28e8a4f
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-12-19 23:00:11 -0500

    Message gonna look like that json boi

--- src/main/java/com/alexjamesmalcolm/dishbot/Message.java
+++ src/main/java/com/alexjamesmalcolm/dishbot/Message.java
@@ -2,37 +2,112 @@ package com.alexjamesmalcolm.dishbot;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.util.Map;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.Timestamp;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
 @Entity
 public class Message {
 
+//    {
+//        "attachments": [],
+//        "avatar_url": "https://i.groupme.com/123456789",
+//            "created_at": 1302623328,
+//            "group_id": "1234567890",
+//            "id": "1234567890",
+//            "name": "John",
+//            "sender_id": "12345",
+//            "sender_type": "user",
+//            "source_guid": "GUID",
+//            "system": false,
+//            "text": "Hello world ☃☃",
+//            "user_id": "1234567890"
+//    }
+
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private long id;
 
-    private String senderName;
-    private String content;
-    private String attachments;
+    private String name;
+    private String source_guid;
+    private String sender_type;
+    private Iterable<String> attachments;
+    private URL avatar_url;
+    private String text;
+    private boolean system;
+    private Timestamp created_at;
+    private long group_id;
+    private long sender_id;
+    private long user_id;
 
-    private Message() {}
-
-    public Message(Map<String, String> map) {
-        senderName = map.get("name");
-        content = map.get("text");
-        attachments = map.get("attachments");
+    private Message() {
     }
 
-    public String getSenderName() {
-        return senderName;
+    public Message(Iterable<String> attachments, String avatar_url, long created_at, String group_id, String id, String name,
+                   String sender_id, String sender_type, String source_guid, boolean system, String text, String user_id) {
+        this.id = Long.parseLong(id);
+        this.name = name;
+        this.text = text;
+        this.attachments = attachments;
+        try {
+            this.avatar_url = new URL(avatar_url);
+        } catch (MalformedURLException e) {
+            e.fillInStackTrace();
+        }
+        this.sender_type = sender_type;
+        this.source_guid = source_guid;
+        this.system = system;
+        this.created_at = new Timestamp(created_at);
+        this.group_id = Long.parseLong(group_id);
+        this.sender_id = Long.parseLong(sender_id);
+        this.user_id = Long.parseLong(user_id);
     }
 
-    public String getContent() {
-        return content;
+    public String getName() {
+        return name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getSourceGuid() {
+        return source_guid;
+    }
+
+    public long getGroupId() {
+        return group_id;
+    }
+
+    public String getSenderType() {
+        return sender_type;
+    }
+
+    public Iterable<String> getAttachments() {
+        return attachments;
+    }
+
+    public URL getAvatarUrl() {
+        return avatar_url;
+    }
+
+    public Timestamp getCreatedAt() {
+        return created_at;
+    }
+
+    public long getSenderId() {
+        return sender_id;
+    }
+
+    public long getUserId() {
+        return user_id;
+    }
+
+    public boolean isSystem() {
+        return system;
     }
 
     public long getId() {
--- src/main/java/com/alexjamesmalcolm/dishbot/MessageController.java
+++ src/main/java/com/alexjamesmalcolm/dishbot/MessageController.java
@@ -5,14 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toMap;
 
 @RestController
 public class MessageController {
@@ -21,12 +13,7 @@ public class MessageController {
     private MessageRepository messageRepo;
 
     @RequestMapping("/receive-message")
-    public void receiveMessage(HttpServletRequest request) throws IOException {
-        String json = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
-        String withTheEndsCutOff = json.substring(1, json.length() - 1).replace("\"", "");
-        Stream<String> stream = Arrays.stream(withTheEndsCutOff.split(","));
-        Map<String, String> map = stream.collect(toMap(x -> x.split(":")[0], x -> x.split(":")[1]));
-        Message message = new Message(map);
+    public void receiveMessage(Message message) {
         messageRepo.save(message);
     }
 
