7c4e5987151b6b12a691cb6d6328cef5c1889f45
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-12-22 19:32:32 -0500

    Whoops, Transactional, not Transient

--- src/main/java/com/alexjamesmalcolm/dishbot/MessageController.java
+++ src/main/java/com/alexjamesmalcolm/dishbot/MessageController.java
@@ -1,14 +1,13 @@
 package com.alexjamesmalcolm.dishbot;
 
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import sun.awt.image.SurfaceManager;
 
 import javax.annotation.Resource;
 import javax.persistence.EntityManager;
-import javax.persistence.Transient;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
@@ -21,7 +20,7 @@ public class MessageController {
     @Resource
     private EntityManager em;
 
-    @Transient
+    @Transactional
     @RequestMapping("/receive-message")
     @SendTo("https://api.groupme.com/v3/bots/post")
     public BotMessage receiveMessage(HttpServletRequest request) throws IOException, SystemMessageException {
