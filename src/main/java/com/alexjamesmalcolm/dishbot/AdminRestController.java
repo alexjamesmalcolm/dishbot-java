1f31ba872dc8fc16e181ad6a3a1b9b7c24b8007b
Alexander James Malcolm <ajmalcolm@aep.com>
2019-03-05 11:27:49 -0500

    Added method for adding users to wheels

--- src/main/java/com/alexjamesmalcolm/dishbot/AdminRestController.java
+++ src/main/java/com/alexjamesmalcolm/dishbot/AdminRestController.java
@@ -2,9 +2,11 @@ package com.alexjamesmalcolm.dishbot;
 
 import com.alexjamesmalcolm.dishbot.exception.NotFoundException;
 import com.alexjamesmalcolm.dishbot.physical.Group;
+import com.alexjamesmalcolm.dishbot.physical.User;
 import com.alexjamesmalcolm.dishbot.physical.Wheel;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -19,9 +21,12 @@ public class AdminRestController {
     @Resource
     private GroupRepository groupRepo;
 
+    @Resource
+    private UserRepository userRepo;
+
     @PostMapping("/group/{groupId}/wheel")
     public void createWheelForGroup(@PathVariable Long groupId, HttpServletResponse response) {
-        Group group = groupRepo.findById(groupId).orElseThrow(() -> new NotFoundException("Group not found."));
+        Group group = findGroup(groupId);
         Wheel wheel = group.getWheel();
         if (wheel == null) {
             group.createWheel();
@@ -30,5 +35,22 @@ public class AdminRestController {
         } else {
             response.setStatus(OK.value());
         }
+    }
+
+    @PostMapping("/group/{groupId}/wheel")
+    public void addUserToWheel(@PathVariable Long groupId, @RequestParam Long userId, HttpServletResponse response) {
+        Group group = findGroup(groupId);
+        User user = findUser(userId);
+        Wheel wheel = group.getWheel();
+        wheel.addRoommate(user);
+        groupRepo.save(group);
+    }
+
+    private Group findGroup(Long groupId) {
+        return groupRepo.findById(groupId).orElseThrow(() -> new NotFoundException("Group not found."));
+    }
+
+    private User findUser(Long userId) {
+        return userRepo.findById(userId).orElseThrow(() -> new NotFoundException("User not found."));
     }
 }
