859a5ed6b6f41e42694fdf51f3a71ef13de7f507
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2019-03-06 01:54:44 -0500

    Getting ready to implement the interpreter

--- src/main/java/com/alexjamesmalcolm/dishbot/MessageController.java
+++ src/main/java/com/alexjamesmalcolm/dishbot/MessageController.java
@@ -2,20 +2,21 @@ package com.alexjamesmalcolm.dishbot;
 
 import com.alexjamesmalcolm.dishbot.bean.GroupMeService;
 import com.alexjamesmalcolm.dishbot.bean.Interpreter;
+import com.alexjamesmalcolm.dishbot.groupme.Group;
 import com.alexjamesmalcolm.dishbot.groupme.Message;
+import com.alexjamesmalcolm.dishbot.logical.BotMessage;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import javax.persistence.EntityManager;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class MessageController {
-
-    @Resource
-    private EntityManager em;
 
     @Resource
     private GroupMeService groupMe;
@@ -26,11 +27,16 @@ public class MessageController {
     @Transactional
     @RequestMapping("/receive-message")
     public void receiveMessage(Message message) {
-        System.out.println(message);
+        BotMessage response = interpreter.respond(message);
+        groupMe.sendMessage(response);
     }
 
     @GetMapping("/message")
     public Iterable<Message> getMessages() {
-        return null;
+        List<Group> groups = groupMe.getAllGroups();
+        return groups.stream().map(group -> {
+            long groupId = group.getGroup_id();
+            return groupMe.getMessages(groupId);
+        }).flatMap(Collection::stream).collect(Collectors.toList());
     }
 }
--- src/main/java/com/alexjamesmalcolm/dishbot/bean/Interpreter.java
+++ src/main/java/com/alexjamesmalcolm/dishbot/bean/Interpreter.java
@@ -1,7 +1,12 @@
 package com.alexjamesmalcolm.dishbot.bean;
 
+import com.alexjamesmalcolm.dishbot.groupme.Message;
+import com.alexjamesmalcolm.dishbot.logical.BotMessage;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Interpreter {
+    public BotMessage respond(Message message) {
+        return null;
+    }
 }
