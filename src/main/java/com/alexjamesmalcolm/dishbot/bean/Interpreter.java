53b65a2d73ed599e341d29b4e72f94360a8adc73
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2019-03-07 17:19:59 -0500

    Refactored two private methods for readability

--- src/main/java/com/alexjamesmalcolm/dishbot/bean/Interpreter.java
+++ src/main/java/com/alexjamesmalcolm/dishbot/bean/Interpreter.java
@@ -24,26 +24,34 @@ public class Interpreter {
     public Optional<String> respond(Message message) {
         String text = message.getText();
         if (text.equals("!Dishes")) {
-            Wheel wheel = wheelRepo.findByGroupId(message.getGroupId());
-            long currentMemberUserId = wheel.getCurrentMemberUserId();
-            wheel.advanceWheel();
-            long nextMemberUserId = wheel.getCurrentMemberUserId();
-            wheelRepo.save(wheel);
-            Group group = groupMe.getGroup(message);
-            String firstName = group.getMember(currentMemberUserId).get().getName();
-            String secondName = group.getMember(nextMemberUserId).get().getName();
-            return Optional.of(MessageFormat.format("Thank you for cleaning the dishes {0}! The next person on dishes is {1}.", firstName, secondName));
+            return dishesDoneCommand(message);
         } else if (text.equals("!Time")) {
-            long userId = message.getUserId();
-            Group group = groupMe.getGroup(message);
-            Optional<Member> potentialMember = group.getMember(userId);
-            Member member = potentialMember.get();
-            String name = member.getName();
-            Wheel wheel = wheelRepo.findByGroupId(message.getGroupId());
-            Duration durationUntilFine = wheel.getDurationUntilFineForCurrent();
-            long hours = durationUntilFine.toHours();
-            return Optional.of(name + " has " + hours + " hours to do the dishes.");
+            return timeLeftCommand(message);
         }
         return Optional.empty();
+    }
+
+    private Optional<String> timeLeftCommand(Message message) {
+        long userId = message.getUserId();
+        Group group = groupMe.getGroup(message);
+        Optional<Member> potentialMember = group.getMember(userId);
+        Member member = potentialMember.get();
+        String name = member.getName();
+        Wheel wheel = wheelRepo.findByGroupId(message.getGroupId());
+        Duration durationUntilFine = wheel.getDurationUntilFineForCurrent();
+        long hours = durationUntilFine.toHours();
+        return Optional.of(name + " has " + hours + " hours to do the dishes.");
+    }
+
+    private Optional<String> dishesDoneCommand(Message message) {
+        Wheel wheel = wheelRepo.findByGroupId(message.getGroupId());
+        long currentMemberUserId = wheel.getCurrentMemberUserId();
+        wheel.advanceWheel();
+        long nextMemberUserId = wheel.getCurrentMemberUserId();
+        wheelRepo.save(wheel);
+        Group group = groupMe.getGroup(message);
+        String firstName = group.getMember(currentMemberUserId).get().getName();
+        String secondName = group.getMember(nextMemberUserId).get().getName();
+        return Optional.of(MessageFormat.format("Thank you for cleaning the dishes {0}! The next person on dishes is {1}.", firstName, secondName));
     }
 }
