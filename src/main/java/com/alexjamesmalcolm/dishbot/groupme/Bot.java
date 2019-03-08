39a392c61697f3933b91a45244345ee7b136abbe
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2019-03-08 13:16:51 -0500

    Made all setters private and renamed getters

--- src/main/java/com/alexjamesmalcolm/dishbot/groupme/Bot.java
+++ src/main/java/com/alexjamesmalcolm/dishbot/groupme/Bot.java
@@ -19,15 +19,15 @@ public class Bot {
         return name;
     }
 
-    public void setName(String name) {
+    private void setName(String name) {
         this.name = name;
     }
 
-    public Long getBot_id() {
+    public long getBotId() {
         return parseLong(bot_id);
     }
 
-    public void setBot_id(String bot_id) {
+    private void setBot_id(String bot_id) {
         this.bot_id = bot_id;
     }
 
@@ -35,39 +35,39 @@ public class Bot {
         return parseLong(group_id);
     }
 
-    public void setGroup_id(String group_id) {
+    private void setGroup_id(String group_id) {
         this.group_id = group_id;
     }
 
-    public String getGroup_name() {
+    public String getGroupName() {
         return group_name;
     }
 
-    public void setGroup_name(String group_name) {
+    private void setGroup_name(String group_name) {
         this.group_name = group_name;
     }
 
-    public String getAvatar_url() {
+    public String getAvatarUrl() {
         return avatar_url;
     }
 
-    public void setAvatar_url(String avatar_url) {
+    private void setAvatar_url(String avatar_url) {
         this.avatar_url = avatar_url;
     }
 
-    public String getCallback_url() {
+    public String getCallbackUrl() {
         return callback_url;
     }
 
-    public void setCallback_url(String callback_url) {
+    private void setCallback_url(String callback_url) {
         this.callback_url = callback_url;
     }
 
-    public String getDm_notification() {
+    public String getDmNotification() {
         return dm_notification;
     }
 
-    public void setDm_notification(String dm_notification) {
+    private void setDm_notification(String dm_notification) {
         this.dm_notification = dm_notification;
     }
 }
