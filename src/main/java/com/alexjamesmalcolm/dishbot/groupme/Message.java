baaff75d7dc829f965a648e34179358beb615c2b
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2019-03-10 15:13:26 -0400

    Reverted upgrading Message's constructor

--- src/main/java/com/alexjamesmalcolm/dishbot/groupme/Message.java
+++ src/main/java/com/alexjamesmalcolm/dishbot/groupme/Message.java
@@ -1,98 +1,159 @@
 package com.alexjamesmalcolm.dishbot.groupme;
 
-import com.alexjamesmalcolm.dishbot.groupme.attachment.*;
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.net.URI;
-import java.time.Instant;
 import java.util.List;
-import java.util.Map;
 
 import static java.lang.Long.parseLong;
-import static java.util.stream.Collectors.toList;
 
-public class Message {
+public class Message extends Response {
 
-    private List<Attachment> attachments;
-    private URI avatarUrl;
-    private Instant createdAt;
-    private List<Long> favoritedBy;
-    private Long groupId;
-    private Long id;
+    private List attachments;
+    private String avatar_url;
+    private String created_at;
+    private List favorited_by;
+    private String group_id;
+    private String id;
     private String name;
-    private Long senderId;
-    private String senderType;
-    private String sourceGuid;
-    private Boolean system;
+    private String sender_id;
+    private String sender_type;
+    private String source_guid;
+    private String system;
     private String text;
-    private Long userId;
+    private String user_id;
     private String platform;
 
-    private Message() {}
-
-    @JsonCreator
+//    @JsonCreator
     private Message(
-            @JsonProperty("attachments") List<Map> attachments,
-            @JsonProperty("avatar_url") String avatarUrl,
-            @JsonProperty("created_at") Integer createdAt,
-            @JsonProperty("favorited_by") List<String> favoritedBy,
-            @JsonProperty("group_id") String groupId,
-            @JsonProperty("id") String id,
-            @JsonProperty("name") String name,
-            @JsonProperty("sender_id") String senderId,
-            @JsonProperty("sender_type") String senderType,
-            @JsonProperty("source_guid") String sourceGuid,
-            @JsonProperty("system") Boolean system,
-            @JsonProperty("text") String text,
-            @JsonProperty("user_id") String userId,
-            @JsonProperty("platform") String platform
-    ) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        System.out.println(attachments);
-        this.attachments = attachments.stream().map(attachment -> {
-            String type = (String) attachment.get("type");
-            switch (type) {
-                case "image":
-                    return objectMapper.convertValue(attachment, Image.class);
-                case "location":
-                    return objectMapper.convertValue(attachment, Location.class);
-                case "split":
-                    return objectMapper.convertValue(attachment, Split.class);
-                case "emoji":
-                    return objectMapper.convertValue(attachment, Emoji.class);
-            }
-            return null;
-        }).collect(toList());
-        this.avatarUrl = parseToUri(avatarUrl);
-        this.createdAt = Instant.ofEpochMilli(createdAt);
-        this.favoritedBy = favoritedBy.stream().map(Long::parseLong).collect(toList());
-        this.groupId = parseLong(groupId);
-        this.id = parseLong(id);
+            List attachments,
+            String avatarUrl,
+            String createdAt,
+            List favoritedBy,
+            String groupId,
+            String id,
+            String name,
+            String senderId,
+            String senderType,
+            String sourceGuid,
+            String system,
+            String text,
+            String userId,
+            String platform
+    ) {}
+
+    private Message() {
+    }
+
+    public URI getAvatar_url() {
+        return URI.create(avatar_url);
+    }
+
+    private void setAvatar_url(String avatar_url) {
+        this.avatar_url = avatar_url;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    private void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public long getGroupId() {
+        return parseLong(group_id);
+    }
+
+    private void setGroup_id(String group_id) {
+        this.group_id = group_id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    private void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
         this.name = name;
-        this.senderId = parseLong(senderId);
-        this.senderType = senderType;
-        this.sourceGuid = sourceGuid;
+    }
+
+    public String getSender_id() {
+        return sender_id;
+    }
+
+    private void setSender_id(String sender_id) {
+        this.sender_id = sender_id;
+    }
+
+    public String getSender_type() {
+        return sender_type;
+    }
+
+    private void setSender_type(String sender_type) {
+        this.sender_type = sender_type;
+    }
+
+    public String getSource_guid() {
+        return source_guid;
+    }
+
+    private void setSource_guid(String source_guid) {
+        this.source_guid = source_guid;
+    }
+
+    public String getSystem() {
+        return system;
+    }
+
+    private void setSystem(String system) {
         this.system = system;
-        this.text = text;
-        this.userId = parseLong(userId);
-        this.platform = platform;
-    }
-
-    private URI parseToUri(String uri) {
-        return uri != null && !uri.isEmpty() ? URI.create(uri) : null;
-    }
-
-    public Long getGroupId() {
-        return groupId;
     }
 
     public String getText() {
         return text;
     }
 
-    public Long getUserId() {
-        return userId;
+    private void setText(String text) {
+        this.text = text;
+    }
+
+    public long getUserId() {
+        return parseLong(user_id);
+    }
+
+    private void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    private void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public List getAttachments() {
+        return attachments;
+    }
+
+    private void setAttachments(List attachments) {
+        this.attachments = attachments;
+    }
+
+    public List getFavorited_by() {
+        return favorited_by;
+    }
+
+    private void setFavorited_by(List favorited_by) {
+        this.favorited_by = favorited_by;
     }
 }
