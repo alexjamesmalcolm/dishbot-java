133e607e442a1129bfdf00ae2a3f6b8b0601d436
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2019-03-18 11:14:16 -0400

    Used mocks to get mockMvcTest working

--- src/test/java/com/alexjamesmalcolm/dishbot/controller/AccountMockMvcTest.java
+++ src/test/java/com/alexjamesmalcolm/dishbot/controller/AccountMockMvcTest.java
@@ -2,11 +2,14 @@ package com.alexjamesmalcolm.dishbot.controller;
 
 import com.alexjamesmalcolm.dishbot.AccountRepository;
 import com.alexjamesmalcolm.dishbot.physical.Account;
+import com.alexjamesmalcolm.groupme.response.Group;
+import com.alexjamesmalcolm.groupme.response.Me;
+import com.alexjamesmalcolm.groupme.service.GroupMeService;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,8 +18,12 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -31,8 +38,17 @@ public class AccountMockMvcTest {
     @Mock
     private Account account;
 
+    @Mock
+    private Me me;
+
+    @Mock
+    private Group group;
+
     @MockBean
     private AccountRepository accountRepo;
+
+    @MockBean
+    private GroupMeService groupMe;
 
     @Before
     public void setup() {
@@ -42,7 +58,11 @@ public class AccountMockMvcTest {
     @Test
     public void shouldGetSettings() throws Exception {
         long userId = 1278939L;
-        Mockito.when(accountRepo.findByUserId(userId)).thenReturn(Optional.of(account));
-        mvc.perform(get("/account/" + userId)).andExpect(status().isOk());
+        String token = "asdf1234";
+        when(accountRepo.findByUserId(userId)).thenReturn(Optional.of(account));
+        when(groupMe.getMe(token)).thenReturn(me);
+        List<Group> groups = Collections.singletonList(group);
+        when(groupMe.getAllGroups(token)).thenReturn(groups);
+        mvc.perform(get("/account/" + userId + "?token=" + token)).andExpect(status().isOk());
     }
 }
