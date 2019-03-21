ddafd6974cf5bb552ef02c26fc0c18ea14f5eaf1
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2019-03-21 11:17:35 -0400

    Switched AccountBeanInjectionTest to not use mocks cause that's cheating

--- src/test/java/com/alexjamesmalcolm/dishbot/physical/AccountBeanInjectionTest.java
+++ src/test/java/com/alexjamesmalcolm/dishbot/physical/AccountBeanInjectionTest.java
@@ -1,49 +1,45 @@
 package com.alexjamesmalcolm.dishbot.physical;
 
-import com.alexjamesmalcolm.groupme.response.Me;
-import com.alexjamesmalcolm.groupme.service.GroupMeService;
-import org.hamcrest.Matchers;
-import org.junit.Assert;
+import com.alexjamesmalcolm.groupme.response.Group;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.mockito.Mockito.when;
+import java.util.Collection;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AccountBeanInjectionTest {
 
-    @InjectMocks
     private Account underTest;
 
-    @MockBean
-    private GroupMeService groupMe;
     private String token;
-
-    @Mock
-    private Me me;
+    private Long userId;
 
     @Before
     public void setup() {
-        MockitoAnnotations.initMocks(this);
-        token = "asdf";
-        when(groupMe.getMe(token)).thenReturn(me);
+        token = "1da573f0282d013738075ad8d3cabcd7";
+        userId = 19742906L;
     }
 
     @Test
     public void shouldGetName() {
-        Long userId = 1L;
         underTest = new Account(token, userId);
         String expected = "Alex";
-        when(me.getName()).thenReturn(expected);
         String name = underTest.getName();
-        Assert.assertThat(name, Matchers.is(expected));
+        assertThat(name, is(expected));
+    }
+
+    @Test
+    public void shouldGetGroupsWithoutWheels() {
+        underTest = new Account(token, userId);
+        Collection<Group> groups = underTest.getGroupsWithoutWheels();
+        assertTrue(groups.size() > 0);
     }
 }
